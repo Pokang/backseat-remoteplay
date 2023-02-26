bb516b9dbb0f797452f9601ceb7c31b39ad39b0e
Raphael Boishus <31265372+Pokang@users.noreply.github.com>
2023-02-26 14:34:04 +0100

    router works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { RemoteComponent } from './remote/remote.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth-component', component: AuthComponent},
+  { path: 'remote-component', component: RemoteComponent},
+
+  { path: '**', component: AuthComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
