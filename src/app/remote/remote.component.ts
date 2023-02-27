23d68f3736dd9fa86e1ce8cefb5da50aeed759c9
Raphael Boishus <31265372+Pokang@users.noreply.github.com>
2023-02-27 17:23:59 +0100

    added all icons, placed them on app

--- src/app/remote/remote.component.ts
+++ src/app/remote/remote.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class RemoteComponent implements OnInit {
 
   constructor() { }
+  toggle:boolean = true;
+
+
+  change(){
+    this.toggle = !this.toggle;
+  } //This needs to take into account that only one dial can be pressed at once
 
   ngOnInit(): void {
   }
